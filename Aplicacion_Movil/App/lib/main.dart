238f3269b94fc03490c12b6752c6101995ae25f9
julio763284 <julio763284@gmail.com>
2026-02-08 20:41:23 -0500

    Actualizacion 1 de la vista Home

--- Aplicacion_Movil/App/lib/main.dart
+++ Aplicacion_Movil/App/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:gestor/HomePage.dart';
 
 void main() {
   runApp(InventaryMobile());
@@ -13,62 +14,3 @@ class InventaryMobile extends StatelessWidget {
   }
 }
 
-class HomePage extends StatelessWidget {
-  const HomePage({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: const Color.fromARGB(255, 1, 122, 116),
-        title: Text(
-          "INVENTARY MOBILE",
-          style: TextStyle(color: Colors.white, fontSize: 20.0),
-        ),
-      ),
-      body: Container(
-        margin: EdgeInsets.all(30.0),
-        padding: EdgeInsets.only(top: 50.0),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Column(
-                  children: [
-                    Container(
-                  width: 150.0 ,
-                  height: 150.0,
-                  decoration: BoxDecoration(
-                    color: const Color.fromARGB(255, 1, 122, 116),
-                    image: DecorationImage(image: AssetImage('assets/iconoGestorProductos.png'))
-                  ),
-                  ),
-                  SizedBox(height: 50.0,),
-                  Container(
-                  width: 150.0,
-                  height: 150.0,
-                  color: const Color.fromARGB(255, 1, 122, 116),
-                ),
-                  ],
-                ),
-                SizedBox(width: 80.0),
-                Column(
-                  children: [
-                    Container(
-                  width: 150.0 ,
-                  height: 150.0,
-                  color: const Color.fromARGB(255, 1, 122, 116),
-                  ),
-                SizedBox(height: 50.0,),
-                  Container(
-                  width: 150.0,
-                  height: 150.0,
-                  color: const Color.fromARGB(255, 1, 122, 116),
-                ),
-                  ],                  
-                )
-              ],
-        ),
-      ),
-    );
-  }
-}
