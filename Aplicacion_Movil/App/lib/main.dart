1ca36b71709f28e6eeeed513547fd00ec17068d8
jholian10 <juandavidrhenalsmontero@gmail.com>
2026-02-26 11:06:10 -0500

    Se coloco botón
    Se coloco botón reenviar codigo

--- Aplicacion_Movil/App/lib/main.dart
+++ Aplicacion_Movil/App/lib/main.dart
@@ -10,11 +10,10 @@ import 'package:gestor/Presentacion/Widgets/GestionarReportes.dart';
 import 'package:gestor/Presentacion/Widgets/notificationView.dart';
 import 'package:gestor/Presentacion/Widgets/vistaDeRegistrarse.dart';
 import 'package:gestor/Presentacion/Widgets/login.dart';
-import 'package:gestor/Presentacion/Widgets/Loading.dart';
 import 'package:gestor/Presentacion/Widgets/olvidar_contrasena.dart';
 import 'package:gestor/Presentacion/Widgets/olvidar_contrasena2.dart';
 import 'package:gestor/Presentacion/Widgets/Cliente.dart';
-
+  
 void main() {
   runApp(const InventaryMobile());
 }
@@ -26,7 +25,7 @@ class InventaryMobile extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: LoginPage(), 
+      home:OlvidarContrasenaPage(), 
     );
   }
 }
