0ee6de915b0fd3e97c00f68a449f30467f192e88
Julio_Martinez <julio763284@gmail.com>
2026-03-08 14:34:56 -0500

    Creacion de los estados del bloc Home
    * se crearon las clases de el archivo estados

---
+++ Aplicacion_Movil/App/lib/bloc/blocHome/Home_state.dart
@@ -0,0 +1,12 @@
+abstract class HomeState {}
+
+class HomeInitial extends HomeState {}
+class GestionarProductos extends HomeState {}
+class GestionarReportes extends HomeState {}
+class VisualizarStock extends HomeState {}
+class GestionarCliente extends HomeState {}
+class GestionarProveedor extends HomeState {}
+class RevisarAlertas extends HomeState {}
+class ControlarFinanzas extends HomeState {}
+class GestionarInventario extends HomeState {}
+class Configuracion extends HomeState {}
