499012e35534d473eb6443bacbd696aee41fa2fa
Juanrhec904 <juandavidrhenalsmontero@gmail.com>
2026-04-14 09:24:06 -0500

    Actualización

--- Aplicacion_Movil/App/lib/main.dart
+++ Aplicacion_Movil/App/lib/main.dart
@@ -4,6 +4,7 @@ import 'package:gestor/HomePage2.dart';
 import 'package:gestor/HomePage.dart';
 import 'package:gestor/Presentacion/Widgets/login.dart';
 import 'package:gestor/Presentacion/Widgets/olvidar_contrasena.dart';
+import 'package:gestor/Presentacion/Widgets/vistaDeRegistrarse.dart';
 import 'package:gestor/bloc/autenticacion/bloc_autenticacion.dart';
 
 void main() {
@@ -19,7 +20,7 @@ class InventaryMobile extends StatelessWidget {
       providers: [BlocProvider(create: (context) => AutenticacionBloc())],
       child: const MaterialApp(
         debugShowCheckedModeBanner: false,
-        home: Homepage2(),
+        home: RegisterView(),
       ),
     );
   }
