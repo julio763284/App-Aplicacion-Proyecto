cf1e94831903ec3d56896d9086cdfd2b10ec7be4
jholian <jholianmanuel10@gmail.com>
2026-04-23 08:55:08 -0500

    login repetido se queda el nuevo

--- Aplicacion_Movil/App/lib/main.dart
+++ Aplicacion_Movil/App/lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:gestor/Presentacion/Widgets/login.dart';
+import 'package:gestor/Presentacion/Pages/LoginHome.dart';
 import 'package:gestor/Presentacion/Widgets/vistaDeRegistrarse.dart';
 import 'package:gestor/bloc/autenticacion/bloc_autenticacion.dart';
 
@@ -17,7 +17,7 @@ class InventaryMobile extends StatelessWidget {
       providers: [BlocProvider(create: (context) => AutenticacionBloc())],
       child: const MaterialApp(
         debugShowCheckedModeBanner: false,
-        home: RegisterView(),
+        home: LoginPage(),
       ),
     );
   }
