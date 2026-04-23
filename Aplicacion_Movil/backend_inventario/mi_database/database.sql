fb51a0d91d6ff20e8578fa92aefa3bb707320470
jholian <jholianmanuel10@gmail.com>
2026-04-23 10:32:36 -0500

    Error de tablas

--- Aplicacion_Movil/backend_inventario/mi_database/database.sql
+++ Aplicacion_Movil/backend_inventario/mi_database/database.sql
@@ -1,23 +1,24 @@
-CREATE DATABASE db_inventario;
+CREATE DATABASE IF NOT EXISTS db_inventario;
+USE db_inventario;
 
-CREATE TABLE usuario(
-    id_usuario AUTO INCREMENT PRIMARY KEY,
+CREATE TABLE usuario (
+    id_usuario INT AUTO_INCREMENT PRIMARY KEY,
     usuario VARCHAR(255) NOT NULL,
     email VARCHAR(255) NOT NULL UNIQUE,
     contrasena VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE producto (
-    id_producto AUTO INCREMENT PRIMARY KEY,
+    id_producto INT AUTO_INCREMENT PRIMARY KEY,
     nombre VARCHAR(255) NOT NULL,
     descripcion TEXT,
     precio DECIMAL(10, 2) NOT NULL,
     cantidad INT NOT NULL,
-    imgen VARCHAR(255)
+    imagen VARCHAR(255) 
 );
 
 CREATE TABLE pedido (
-    id_pedido AUTO INCREMENT PRIMARY KEY,
+    id_pedido INT AUTO_INCREMENT PRIMARY KEY,
     fecha TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     estado ENUM('pendiente', 'enviado', 'entregado') NOT NULL
 );
