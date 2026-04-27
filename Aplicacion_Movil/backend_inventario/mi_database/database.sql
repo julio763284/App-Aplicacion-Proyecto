4e051706e803fe77f66446f37f406a99301888e4
jholian <jholianmanuel10@gmail.com>
2026-04-26 19:29:57 -0500

    Actualización de tabla productos

--- Aplicacion_Movil/backend_inventario/mi_database/database.sql
+++ Aplicacion_Movil/backend_inventario/mi_database/database.sql
@@ -8,24 +8,23 @@ CREATE TABLE usuario (
     contrasena VARCHAR(255) NOT NULL
 );
 
-CREATE TABLE producto (
+CREATE TABLE productos (
     id_producto INT AUTO_INCREMENT PRIMARY KEY,
     nombre VARCHAR(255) NOT NULL,
     descripcion TEXT,
     precio DECIMAL(10, 2) NOT NULL,
-    cantidad INT NOT NULL,
+    cantidad INT NOT NULL DEFAULT 0, 
+    estado ENUM('Disponible', 'Stock Bajo', 'Agotado') DEFAULT 'Agotado',
     imagen VARCHAR(255) 
 );
 
-CREATE TABLE pedido (
+CREATE TABLE pedidos (
     id_pedido INT AUTO_INCREMENT PRIMARY KEY,
     fecha TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     estado ENUM('en espera', 'recibido') NOT NULL
 );
 
-USE db_inventario;
-
-CREATE TABLE cliente (
+CREATE TABLE clientes (
     id_cliente INT AUTO_INCREMENT PRIMARY KEY,
     nombre VARCHAR(255) NOT NULL,
     direccion_residencia VARCHAR(255) NOT NULL,
