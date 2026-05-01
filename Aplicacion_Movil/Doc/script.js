b4e0318f1b6b75c51f1a3fe20f8d6a28afdb67d0
Pacheco05Jesus <pachecobarrios7@gmail.com>
2026-05-01 12:55:55 -0500

    actualizacion#2
    Se le elimina en script.js codigo que no se estaba usando y en styles.css se le hizo midificaciones.

--- Aplicacion_Movil/Doc/script.js
+++ Aplicacion_Movil/Doc/script.js
@@ -1,58 +1,32 @@
-const botonDoc = document.getElementById("btnDoc");
+document.addEventListener("DOMContentLoaded", function () {
 
-botonDoc.addEventListener("click", function(e){
-
-e.preventDefault();
-
-document.getElementById("documentacion")
-.scrollIntoView({
-behavior: "smooth"
-});
-
-});
-
-const botonDiag = document.getElementById("btnDiag");
-
-botonDiag.addEventListener("click", function(e){
-
-e.preventDefault();
-
-document.getElementById("diagramas")
-.scrollIntoView({
-behavior: "smooth"
-});
-
-});
-
-const botonesHome = document.querySelectorAll(".btnHome");
-
-botonesHome.forEach(function(boton){
-
-boton.addEventListener("click", function(e){
-
-e.preventDefault();
-
-document.getElementById("home").scrollIntoView({
-behavior: "smooth"
-});
-
-});
-
-});
-
-const logo = document.getElementById("logo");
-
-document.querySelectorAll("a").forEach(link => {
-  link.addEventListener("click", function(e) {
-    if (this.href && !this.href.includes("#")) {
-      e.preventDefault();
-
-      // Activar animación tipo Google
-      logo.classList.add("loading");
-
-      setTimeout(() => {
-        window.location.href = this.href;
-      }, 400);
-    }
+  VANTA.NET({
+    el: "#index",
+    mouseControls: true,
+    touchControls: true,
+    gyroControls: false,
+    minHeight: 200,
+    minWidth: 200,
+    scale: 1,
+    scaleMobile: 1,
+    color: 0x08f5e9,
+    backgroundColor: 0x0a1517
   });
+
+  const logo = document.getElementById("logo");
+
+  document.querySelectorAll("a").forEach(link => {
+    link.addEventListener("click", function(e) {
+      if (this.href && !this.href.includes("#")) {
+        e.preventDefault();
+
+        logo.classList.add("loading");
+
+        setTimeout(() => {
+          window.location.href = this.href;
+        }, 400);
+      }
+    });
+  });
+
 });
