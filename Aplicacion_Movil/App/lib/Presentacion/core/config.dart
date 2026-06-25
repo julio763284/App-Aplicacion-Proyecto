4049ecb86ee813bd31d0478c764475e5ba0668bb
Juanrhec904 <juandavidrhenalsmontero@gmail.com>
2026-06-25 09:45:12 -0500

    Creacion vista stock
    Creacion vista stock

--- Aplicacion_Movil/App/lib/Presentacion/core/config.dart
+++ Aplicacion_Movil/App/lib/Presentacion/core/config.dart
@@ -1,5 +1,5 @@
 class ApiConfig {
-  static String baseUrl = "http://192.168.1.8:5001";
+  static String baseUrl = "http://10.2.135.49:5001";
 
   static String url(String path) {
     if (path.startsWith('/')) {
